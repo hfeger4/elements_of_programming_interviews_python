4fef26759a2fd862591bf4b7db8e515a355a0cb9
hfeger4 <hfeger4@gmail.com>
2018-03-18 19:26:36 -0700

    add even_odd array

--- epi_judge_python/even_odd_array.py
+++ epi_judge_python/even_odd_array.py
@@ -5,8 +5,15 @@ from test_framework.test_utils import enable_timer_hook
 
 
 def even_odd(A):
-    # Implement this placeholder.
-    return
+    next_even = 0
+    next_odd = len(A) - 1
+    while next_even < next_odd:
+        if A[next_even] % 2 == 0:
+            next_even += 1
+        else:
+            A[next_even], A[next_odd] = A[next_odd], A[next_even]
+            next_odd -= 1
+
 
 
 @enable_timer_hook
