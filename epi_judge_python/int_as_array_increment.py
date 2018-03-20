dfd2a6745c5c544bb264c089b64a15f9eab33b59
hfeger4 <hfeger4@gmail.com>
2018-03-19 17:21:18 -0700

    add int as array increment

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -1,6 +1,17 @@
 def plus_one(A):
-    # Implement this placeholder.
-    return []
+    A[-1] += 1
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i-1] += 1
+
+    if A[0] == 10:
+        A[0] = 1
+        A.append(0)
+
+    return A
+
 
 
 from test_framework import test_utils_generic_main, test_utils
