dc75e9c0832c06cd36846132d4ece51951099a6f
hfeger4 <hfeger4@gmail.com>
2018-03-21 14:24:21 -0700

    add apply permutation

--- epi_judge_python/apply_permutation.py
+++ epi_judge_python/apply_permutation.py
@@ -1,6 +1,12 @@
 def apply_permutation(perm, A):
-    # Implement this placeholder.
-    return
+    for i in range(len(A)):
+        next  = i
+        while perm[next] >= 0:
+            A[i], A[perm[next]] = A[perm[next]], A[i]
+            temp = perm[next]
+            perm[next] -= len(perm)
+            next = temp
+    perm[:] = [a + len(perm) for a in perm]
 
 
 def apply_permutation_wrapper(perm, A):
