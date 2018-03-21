4332209a9ebb78b8edb179808572af6ec7b5667a
hfeger4 <hfeger4@gmail.com>
2018-03-20 18:29:30 -0700

    add generate primes

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -1,7 +1,22 @@
 # Given n, return all primes up to and including n.
+import math
+
 def generate_primes(n):
-    # Implement this placeholder.
-    return []
+    arr = []
+    i = 2
+    while i <= n:
+        if is_prime(i):
+            arr.append(i)
+        i += 1
+    return arr
+
+def is_prime(num):
+    i = 2
+    while i <= math.sqrt(num):
+        if num % i == 0:
+            return False
+        i += 1
+    return True
 
 
 from test_framework import test_utils_generic_main, test_utils
